7fd2cd1fa4b1eac4a72e7a5cf338c3eee2d3800a
LUNAGOK <21lunagok@gmail.com>
2023-03-03 21:26:29 +0530

    fucked it have to learn string function

--- Leetcode Question/tryoutstuff.java
+++ Leetcode Question/tryoutstuff.java
@@ -1,19 +1,63 @@
+import java.util.*;
 public class tryoutstuff {
     public static void main(String[] args) {
+        char[] nums = { 'p', 'q', 'r', 's' };
+        String str = String.valueOf(nums);
+        System.out.println(nums);
+
+
+
+
+
+
+
+
+
+
+
+        // int[] nums = {4,3,2,7,8,2,3,1};
+        // int[] arr = new int[nums.length];
+        // int i;
+        // int j;
+        // for(i=0;i<nums.length;i++){
+        //     for(j=i+1; j< nums.length;j++){
+        //         if(nums[i]== nums[j]){
+        //             arr[j] = nums[j];
+        //         }
+        //     }
+        // }
+        // for (int j2 = 0; j2 < arr.length; j2++) {
+        // //  if (arr[j2]>0) {
+        //     System.out.println(arr[j2]);
+        // //  }
+        //  } 
+        
+    }
+
+}
+
+
+
+
         // int[] arr = {5,8,84,81};
         // for (int i = 0; i < arr.length-1; i++) {
         //     for (int j = i+1; j < arr.length; j++) {
         //         System.out.println(i +"    "+ j);
         //     }
         // }
-        double celsius = 36.50;
-        double[] result = new double[2];
-        double kelvin = celsius + 273.15;
-        double fahrenheit = celsius * 1.80 + 32.00;
-        result[0] = kelvin;
-        result[1] = fahrenheit;
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
-        }
-    }
-}
+
+
+
+
+
+        // double celsius = 36.50;
+        // double[] result = new double[2];
+        // double kelvin = celsius + 273.15;
+        // double fahrenheit = celsius * 1.80 + 32.00;
+        // result[0] = kelvin;
+        // result[1] = fahrenheit;
+        // for (int i = 0; i < result.length; i++) {
+        //     System.out.println(result[i]);
+        // }
+  //  }
+//}
